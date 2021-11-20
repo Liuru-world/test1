f66081e61290232cd245be53500097b2472ff44d
liuru <2249491304@qq.com>
2021-11-20 17:40:26 +0800

    qqqq

--- wms2light/src/main/java/com/ewy/wms2light/controller/UserController.java
+++ wms2light/src/main/java/com/ewy/wms2light/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // 根据id获取用户(此方法可用于测试数据库连接、异常、RESTful风格以及日志)提交改动
+    // 根据id获取用户(此方法可用于测试数据库连接、异常、RESTful风格以及日志)
     @GetMapping("/userById/{id}")
     public HttpReturn getUserById(@PathVariable("id") Integer id){
         try {
