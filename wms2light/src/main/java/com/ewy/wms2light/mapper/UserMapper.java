d8db8fe1e0f867a44d3b1c389caea67e1286cccc
liuru <2249491304@qq.com>
2021-11-22 23:39:52 +0800

    1

--- wms2light/src/main/java/com/ewy/wms2light/mapper/UserMapper.java
+++ wms2light/src/main/java/com/ewy/wms2light/mapper/UserMapper.java
@@ -9,20 +9,21 @@ import java.util.List;
 @Repository
 @Mapper
 public interface UserMapper {
-    @Select("SELECT * FROM W_USER WHERE id = #{id}")
+//    注释掉了注解开发方式
+//    @Select("SELECT * FROM W_USER WHERE id = #{id}")
     public User findById(Integer id);
 
-    @Select("SELECT * FROM W_USER")
+//    @Select("SELECT * FROM W_USER")
     public List<User> findUsers();
 
-    @Insert("INSERT INTO W_USER VALUES(wuser_seq.nextval,#{name},#{password},#{warehouseno},#{phonenumber},#{roleno})")
+//    @Insert("INSERT INTO W_USER VALUES(wuser_seq.nextval,#{name},#{password},#{warehouseno},#{phonenumber},#{roleno})")
     public Integer insertUser(User user);
 
-    @Update("UPDATE W_USER SET name = #{name},password = #{password},warehouseno = #{warehouseno}," +
-            "phonenumber = #{phonenumber},roleno = #{roleno} WHERE id = #{id}")
+//    @Update("UPDATE W_USER SET name = #{name},password = #{password},warehouseno = #{warehouseno}," +
+//            "phonenumber = #{phonenumber},roleno = #{roleno} WHERE id = #{id}")
     public Integer updateUser(User user);
 
-    @Delete("DELETE FROM W_USER WHERE id = #{id}")
+//    @Delete("DELETE FROM W_USER WHERE id = #{id}")
     public Integer deleteUserById(Integer id);
 
 }
